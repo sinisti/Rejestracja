d42ed69117811997d62a3fe2bb0cc85c56f09ac8
sinisti <spideralan2008@gmail.com>
2024-11-25 09:27:33 +0100

    Final Version! UwU

--- app/src/main/java/com/example/rejestracja/MainActivity.java
+++ app/src/main/java/com/example/rejestracja/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rejestracja;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,16 +14,32 @@ public class MainActivity extends AppCompatActivity {
     private EditText email;
     private EditText haslo;
     private EditText haslo2;
+    private TextView editmsg;
     private Button confirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        email = findViewById(R.id.email);
-        haslo = findViewById(R.id.password);
-        haslo2 = findViewById(R.id.password2);
-        confirm = findViewById(R.id.confirm);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        };
+        email= findViewById(R.id.email);
+        haslo = findViewById(R.id.password);
+        haslo2 = findViewById(R.id.password2);
+        editmsg = findViewById(R.id.editmsg);
+        confirm = (Button) findViewById(R.id.confirm);
+        confirm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                confirm();
+            }
+        });
     }
+    private void confirm() {
+        if (!email.getText().toString().contains("@")) {
+            editmsg.setText("Nieprawidłowy adres e-mail");
+        } else if (!haslo.getText().toString().equals(haslo2.getText().toString())) {
+            editmsg.setText("Hasła się różnią");
+        } else {
+            editmsg.setText("Witaj " + email.getText().toString());
+        }
+    }
+}
